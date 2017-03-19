da5820c100757291ddae9f58b9233e16ab92ea63
Stian Eklund <stian.eklund@gmail.com>
2017-03-19 12:33:43 +0100

    Add more instructions. TODO: Generalize functions for reuse.
    Generalize functions so that they can be used for more than one instruction.
    
    Consider creating a own memory management set, outside of CPU and create
    a interconnect to be able to pass without ending up referencing something that's
    no longer in memory.
    
    This code is very messy. Sit down & think through how this can be improved
    before you start hacking away next time.

--- src/cpu.rs
+++ src/cpu.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
+const DEBUG: bool = true;
+
 // Intel 8080 Notes:
 //
 // The Intel 8080 has 7 8-bit registers (A,B,C,D,E,H and L).
@@ -102,26 +104,25 @@ impl Cpu {
         (((word as u8) as u16) << 8) | ((byte as u8) as u16)
     }
 
-      pub fn read_byte(&mut self) -> u8 {
-          // self.memory[addr as usize & 0xFFFF];
-          let byte = self.memory[self.pc as usize];
-          byte
+    // TODO Move this out of cpu.rs & into separate file for cleaner memory management.
+    // Make interconnect for memory management?
+
+    pub fn read_byte(&mut self, addr: u8) -> u8 {
+        self.memory[addr as usize & 0xFFFF]
     }
 
-    pub fn write_byte(&mut self, addr: u8, byte: u8) {
-        self.memory[addr as usize] = byte;
+    pub fn write_byte(&mut self, addr: u8, mut byte: u16) {
+        byte = self.memory[addr as usize & 0xFFFF] as u16;
     }
 
-    pub fn read_word(&mut self) -> u16 {
-        // high byte
-        (self.memory[self.pc as usize] as u16) << 8 |
-        (self.memory[self.pc as usize + 1] as u16)
+    pub fn read_word(&mut self, addr: u8) -> u16 {
+        (self.read_byte(addr + 1) as u16) << 8 | self.read_byte(addr) as u16
     }
 
 
     pub fn write_word(&mut self, addr: u8, word: u16) {
-        self.memory[addr as usize + 1 as usize] = (word >> 8) as u8;
-        self.memory[addr as usize] = word as u8;
+        self.write_byte(addr, word & 0xFF);
+        self.write_byte(addr + 1, (word >> 8) & 0xFF);
     }
 
     pub fn instruction_nop(&mut self) {
@@ -133,30 +134,37 @@ impl Cpu {
     }
 
     pub fn instruction_lxi_sp(&mut self) {
-        // TODO use write fn
-        self.sp = (self.memory[self.pc as usize] as u16) << 8 | (self.memory[self.pc as usize + 1] as u16);
+        self.sp = (self.memory[self.pc as usize] as u16) << 8 |
+        (self.memory[self.pc as usize + 1] as u16);
+
         self.pc += 2;
     }
     pub fn instruction_mvi_a(&mut self) {
-        let byte = self.read_byte();
-        self.reg_a = byte;
+        let byte = self.reg_a;
+        self.read_byte(byte);
+        self.pc += 1;
     }
 
     pub fn instruction_sta(&mut self) {
-        // TODO Rewrite this.. mutable borrow occurs here.
-        match self.opcode {
-            0x02 => {
-                self.write_word(self.reg_a, self.reg_bc);
-            },
-            0x12 => {
-                self.write_word(self.reg_a, self.reg_de);
-            },
-            0x32 => {
-                let byte = self.read_word() as u8;
-                self.write_byte(byte, self.reg_a);
-            },
-            _ => panic!("Unhanled instruction"),
-        }
+        let reg_a = self.reg_a;
+        let reg_bc = self.reg_bc;
+        self.write_word(reg_a, reg_bc);
+    }
+
+    pub fn inc_bc(&mut self) {
+        self.reg_bc += self.reg_bc;
+        self.pc += 1;
+    }
+    pub fn inc_pc(&mut self, amount: u16) {
+        self.pc += amount;
+    }
+
+    pub fn push_b(&mut self) {
+        self.sp.wrapping_sub(2);
+        let sp = self.sp;
+        let b = self.reg_b;
+        self.write_word(b, sp);
+        self.pc += 1;
     }
 
 
@@ -164,18 +172,38 @@ impl Cpu {
         self.opcode = self.memory[self.pc as usize];
         // (self.memory[self.pc as usize] as u16) << 8 | (self.memory[self.pc as usize + 1] as u16)
 
-        println!("Opcode: 0x{:X}, PC: {}, SP: {}", self.opcode, self.pc, self.sp);
+        if DEBUG { println!("Opcode: 0x{:X}, PC: {}, SP: {}", self.opcode, self.pc, self.sp); }
 
         match self.opcode {
             0x00 | 0x08 | 0x10 | 0x18 | 0x20 | 0x28 | 0x30 | 0x38 =>  self.instruction_nop(),
 
             0x01 => self.instruction_lxi_sp(),
-
             0x02 => self.instruction_sta(),
-            0x3E => self.instruction_mvi_a(),
-            0x06 =>  {}, // TODO
+            0x03 => self.inc_bc(),
+
+            // All of these are not supposed to be NOP's.
+            0x05 => self.instruction_nop(),
+            0x06 => self.instruction_nop(),
+            0x14 => self.instruction_nop(),
+
+            0xC2 => self.instruction_nop(),
+
             0xC3 =>  self.instruction_jmp(),
-            _ => return
+            0xC5 =>  self.push_b(),
+
+            0xC8 => self.instruction_nop(),
+            0xCD => self.instruction_nop(),
+            0x3C => self.instruction_nop(),
+            0x3D => self.instruction_nop(),
+
+            0x3E => self.instruction_mvi_a(),
+
+            0x32 => self.instruction_nop(),
+            0xD3 => self.instruction_nop(),
+            0xFE => self.instruction_nop(),
+            0x74 => self.instruction_nop(),
+
+            _ => println!("Unknown opcode: {:X}", self.opcode),
         }
     }
 
@@ -219,4 +247,3 @@ impl Cpu {
     }
 
 }
-
