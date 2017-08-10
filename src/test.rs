4d07e2a1cdf026dc48393438d81760d73a0d619f
Stian Eklund <stian.eklund@gmail.com>
2017-08-10 12:26:20 +0200

    Load tests at 0x0100 and jump to 0x0100 on test start

--- src/test.rs
+++ src/test.rs
@@ -2,15 +2,30 @@
 mod tests {
     use cpu::{Registers, ExecutionContext};
     use memory::Memory;
+    use std::fs::File;
+    use std::io::prelude::*;
+    use std::path::Path;
 
     #[test]
     fn cpu_test() {
-        // Standup CPU, memory & registers
+        // Standup memory & registers
         let mut memory = Memory::new();
         let mut registers = Registers::new();
 
-        // Load CPUTEST from root directory
-        memory.load_bin("8080PRE.COM");
+        // 8080PRE
+        let path = Path::new("8080PRE.COM");
+        let mut file = File::open(&path).expect("Couldn't load binary");
+        let mut buf = Vec::new();
+
+
+        file.read_to_end(&mut buf).expect("Failed to read binary");
+        let buf_len = buf.len();
+        for i in 0..buf_len {
+
+            memory.memory[i + 0x0100] = buf[i];
+        }
+        println!("Loaded: {:?} Bytes: {:?}", path, buf_len);
+
 
         let mut cpu = ExecutionContext::new(&mut memory, &mut registers);
 
@@ -19,20 +34,22 @@ mod tests {
         cpu.memory.memory[5] = 0xC9;
         // println!("Setting return address: [5]: {:#04X}", cpu.memory.memory[5]);
 
+        // INIT
         // i8080core sets this for i8080_init().. Why?
-        // cpu.registers.pc = 0xF800;
+        cpu.registers.pc = 0xF800;
 
         // TODO Look at what start addresses the other CPU tests need.
         // Commented out as we're using 8080PRE as our test
         // For CPUTEST Only
-        // cpu.registers.pc = 0x100;
+        cpu.registers.pc = 0x0100;
         // println!("Jumping to: {:#04X}", cpu.registers.pc);
 
         let mut success: bool = false;
-
+        let mut instruction = cpu.memory.read(cpu.registers.pc as usize);
         for i in 0..10 {
-            cpu.step(1);
-            if cpu.registers.pc == 0x76 {
+            cpu.execute_instruction(instruction);
+            // cpu.step(1);
+           if cpu.registers.pc == 0x76 {
                 println!("HALT at {:#04X}", cpu.registers.pc);
                 break
             }
