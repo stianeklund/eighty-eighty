55850be9b652129c27d07aa5133b61a59aa1ba4f
Stian Eklund <stian.eklund@gmail.com>
2017-11-28 16:16:36 +0100

    TODO Setup use correct bitmask for P2 & DIP settings

--- src/keypad.rs
+++ src/keypad.rs
@@ -2,12 +2,29 @@ use minifb::{Key, Window};
 use cpu::Registers;
 
 pub struct Keypad {
-    p1_start: u8,
-    p2_start: u8,
-    credit: u8,
-    fire: u8,
-    left: u8,
-    right: u8,
+    // DIP settings
+    coin_info: u8, //  Port 2 Bit 7 Coin info displayed in demo screen if 0x00
+    credit: u8,    // Bit 0 1 if coin deposited
+    tilt: u8,      // Tilt check
+    dip3: u8,      // Bit 0 (0x00 = 3 ships, 0x10 = 5 ships)
+    dip5: u8,      // Bit 1 (0x01 = 4 ships, 0x11 = 6 ships)
+    dip6: u8,      // Bit 3 (0x00 = extra ship at 1500, 0x01 = extra ship at 1000)
+
+    // Player 1
+    p1_start: u8,  // Port 1 Bit 1 (0x01 if pressed)
+
+    // Port 0  Bits
+                   // Port 0 Bit 3 (always 0x01)
+    p1_fire: u8,   // Port 0 Bit 4 (0x01 if pressed)
+    p1_left: u8,   // Port 0 Bit 5 (0x01 if pressed)
+    p1_right: u8,  // Port 0 Bit 6 (0x01 if pressed)
+
+    // Player 2
+    p2_shot: u8,   // Port 2 Bit 4 (0x01 if pressed)
+    p2_start: u8,  // Port 1 Bit 1 (0x01 if pressed)
+    p2_right: u8,  // Port 2 Bit 6 (0x01 if pressed)
+    p2_left: u8,   // Port 2 Bit 5 (0x01 if pressed)
+
 }
 
 pub struct State {
@@ -25,12 +42,20 @@ pub trait Input {
 impl Keypad {
     pub fn new() -> Keypad {
         Keypad {
+            coin_info: 0,
             p1_start: 0x04,
+            p1_fire: 0,
+            p1_left: 0,
+            p1_right: 0,
+            p2_shot: 0,
             p2_start: 0x2,
+            p2_left: 0,
+            p2_right: 0,
             credit: 0x1,
-            fire: 0x10,
-            left: 0x20,
-            right: 0x40,
+            tilt: 0,
+            dip3: 0,
+            dip5: 0,
+            dip6: 0,
         }
     }
     pub fn poll_input(registers: &mut Registers, window: &Window) {
@@ -66,12 +91,20 @@ impl State {
 impl Input for Registers {
     fn key_value(&self) -> Keypad {
         Keypad {
+            coin_info: 0,
             p1_start: 0x04,
+            p1_fire: 0,
+            p1_left: 0,
+            p1_right: 0,
+            p2_shot: 0,
             p2_start: 0x2,
+            p2_right: 0,
             credit: 0x1,
-            fire: 0x10,
-            left: 0x20,
-            right: 0x40,
+            tilt: 0,
+            dip3: 0,
+            dip5: 0,
+            dip6: 0,
+            p2_left: 0,
         }
     }
     fn key_down(&mut self, key: Key) {
@@ -81,10 +114,10 @@ impl Input for Registers {
         match key {
             Key::Enter => self.port_1_in |= keypad.p1_start,
             Key::C     => self.port_1_in |= keypad.credit,
-            Key::Space => self.port_1_in |= keypad.fire,
+            Key::Space => self.port_1_in |= keypad.p1_fire,
             Key::Key2  => self.port_2_in |= keypad.p2_start,
-            Key::Left  => self.port_1_in |= keypad.left,
-            Key::Right => self.port_1_in |= keypad.right,
+            Key::Left => self.port_1_in |= keypad.p1_left,
+            Key::Right => self.port_1_in |= keypad.p1_right,
             _ => eprintln!("Key not implemented"),
         }
 
@@ -98,11 +131,11 @@ impl Input for Registers {
 
         match key {
             Key::Enter => self.port_1_in &= keypad.p1_start,
+            Key::Key2 => self.port_2_in &= keypad.p2_start,
             Key::C     => self.port_1_in &= keypad.credit,
-            Key::Space => self.port_1_in &= keypad.fire,
-            Key::Key2  => self.port_2_in &= keypad.p2_start,
-            Key::Left  => self.port_1_in &= keypad.left,
-            Key::Right => self.port_1_in &= keypad.right,
+            Key::Space => self.port_1_in &= keypad.p1_fire,
+            Key::Left => self.port_1_in &= keypad.p1_left,
+            Key::Right => self.port_1_in &= keypad.p1_right,
             _ => eprintln!("Key not implemented"),
         }
 
